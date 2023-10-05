575d6c5775f36fe79233210b2cd920bae5e78593
The-Something <georgetturner9855@gmail.com>
2023-10-05 20:39:38 +0200

    clear

--- Functions/sandbox.py
+++ Functions/sandbox.py
@@ -1,9 +1,2 @@
 #### Playground to test thing before throwing it into a programme
 
-def go_fuck(x):
-    val1 = x + 1
-    return val1
-
-print(go_fuck(1000))
-print(go_fuck(10))
-print(go_fuck(11))
